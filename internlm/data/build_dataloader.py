95f8271bff7943521947328bb515e8571eb2c0f5
Rui Huang <1251433731@qq.com>
2024-04-10 16:39:48 +0800

    feat(multimodal): support train llava with dummy dataset (#91)
    Co-authored-by: huangrui <huangrui@pjlab.org.cn>
    Co-authored-by: JiaoPL <jplzyc@163.com>
    Co-authored-by: jiaopenglong <44927264+JiaoPL@users.noreply.github.com>

--- internlm/data/build_dataloader.py
+++ internlm/data/build_dataloader.py
@@ -13,9 +13,11 @@ from internlm.data.tokenized.batch_sampler import (
 from internlm.data.tokenized.collaters import jsonl_ds_collate_fn, packed_collate_fn
 from internlm.data.tokenized.dataset import get_dataset_dict
 from internlm.data.tokenized.dummy_dataset import RandomDataset
+from internlm.data.tokenized.dummy_dataset_multimodal import RandomDatasetMultimodal
 from internlm.data.tokenized.packed_dataset import (
     PackedDatasetWithCut,
     PackedDatasetWithoutCuSeqlen,
+    PackedDatasetWithPadForMultimodal,
     get_packed_dataset_without_short_length,
 )
 from internlm.data.utils import get_dataset_type_ids_map
@@ -28,18 +30,30 @@ logger = get_logger(__file__)
 def get_tokenized_train_loader_items(data_cfg):
     """Get the training data loader for tokenized dataset."""
     if data_cfg.get("train_folder", None) is None:
-        train_ds = RandomDataset(
-            num_samples=1000000, max_len=data_cfg.seq_len, fixed_seqlen=data_cfg.fixed_random_dataset_seqlen
-        )
-
-        if data_cfg.pack_sample_into_one:
-            train_ds = PackedDatasetWithoutCuSeqlen(
+        if data_cfg.get("is_multimodal", False):
+            image_token_size = int(data_cfg.image_size // data_cfg.patch_size) ** 2
+            train_ds = RandomDatasetMultimodal(
+                num_samples=10000,
+                max_len=data_cfg.seq_len,
+                image_size=data_cfg.image_size,
+                image_token_size=image_token_size,
+            )
+            train_ds = PackedDatasetWithPadForMultimodal(
                 train_ds, max_length_per_sample=data_cfg.seq_len, packed_length=data_cfg.packed_length
             )
         else:
-            train_ds = PackedDatasetWithCut(
-                train_ds, max_length_per_sample=data_cfg.seq_len, packed_length=data_cfg.packed_length
+            train_ds = RandomDataset(
+                num_samples=1000000, max_len=data_cfg.seq_len, fixed_seqlen=data_cfg.fixed_random_dataset_seqlen
             )
+
+            if data_cfg.pack_sample_into_one:
+                train_ds = PackedDatasetWithoutCuSeqlen(
+                    train_ds, max_length_per_sample=data_cfg.seq_len, packed_length=data_cfg.packed_length
+                )
+            else:
+                train_ds = PackedDatasetWithCut(
+                    train_ds, max_length_per_sample=data_cfg.seq_len, packed_length=data_cfg.packed_length
+                )
     else:
         train_ds = get_packed_dataset_without_short_length(
             folder=data_cfg.train_folder,
@@ -69,11 +83,20 @@ def get_tokenized_train_loader_items(data_cfg):
 def get_tokenized_valid_loader_items(data_cfg):
     """Get the validation data loader for tokenized dataset."""
     if not data_cfg.valid_folder:
-        valid_ds = RandomDataset(
-            num_samples=gpc.get_world_size(ParallelMode.DATA) * 500,
-            max_len=data_cfg.seq_len,
-            fixed_seqlen=data_cfg.fixed_random_dataset_seqlen,
-        )
+        if data_cfg.get("is_multimodal", False):
+            image_token_size = int(data_cfg.image_size // data_cfg.patch_size) ** 2
+            valid_ds = RandomDatasetMultimodal(
+                num_samples=gpc.get_world_size(ParallelMode.DATA) * 500,
+                max_len=data_cfg.seq_len,
+                image_size=data_cfg.image_size,
+                image_token_size=image_token_size,
+            )
+        else:
+            valid_ds = RandomDataset(
+                num_samples=gpc.get_world_size(ParallelMode.DATA) * 500,
+                max_len=data_cfg.seq_len,
+                fixed_seqlen=data_cfg.fixed_random_dataset_seqlen,
+            )
     else:
         valid_ds = get_dataset_dict(folder=data_cfg.valid_folder, split="")
 
