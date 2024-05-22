104e6b4ae646f5f4380aad38bd7949bc3e82b963
cx <759046501@qq.com>
2024-05-22 14:58:24 +0800

    feat(attention): add full_kv zigzag (#229)

--- internlm/model/ops/ring_flash_attn/__init__.py
+++ internlm/model/ops/ring_flash_attn/__init__.py
@@ -8,6 +8,11 @@ from .ring_flash_attn_varlen import (
     ring_flash_attn_varlen_kvpacked_func,
     ring_flash_attn_varlen_qkvpacked_func,
 )
+from .stripe_flash_attn import (
+    stripe_flash_attn_func,
+    stripe_flash_attn_kvpacked_func,
+    stripe_flash_attn_qkvpacked_func,
+)
 from .zigzag_ring_flash_attn import (
     zigzag_ring_flash_attn_func,
     zigzag_ring_flash_attn_kvpacked_func,
@@ -18,8 +23,8 @@ from .zigzag_ring_flash_attn_varlen import (
     zigzag_ring_flash_attn_varlen_kvpacked_func,
     zigzag_ring_flash_attn_varlen_qkvpacked_func,
 )
-from .stripe_flash_attn import (
-    stripe_flash_attn_func,
-    stripe_flash_attn_kvpacked_func,
-    stripe_flash_attn_qkvpacked_func,
+from .zigzag_ring_flash_attn_with_full_kv import (
+    zigzag_ring_flash_attn_func_with_full_kv,
+    zigzag_ring_flash_attn_kvpacked_func_with_full_kv,
+    zigzag_ring_flash_attn_qkvpacked_func_with_full_kv,
 )
