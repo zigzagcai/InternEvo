027e87ea53d3d65365300bd518e0f730244b0320
shidongxing <shidongxing@>
2024-05-27 19:13:38 +0800

    update testcase

--- tests/test_infer/test_generate.py
+++ tests/test_infer/test_generate.py
@@ -9,6 +9,16 @@ from internlm.initialize import initialize_distributed_env  # noqa: E402
 from internlm.train import initialize_model, initialize_parallel_communicator
 
 
+def set_seed(seed: int = 1024):
+    import random
+
+    import numpy as np
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+
 def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
     model_cfg = os.path.join(path, "model_config.pt")
     model_wt = os.path.join(path, "model_tp0_pp0.pt")
@@ -53,6 +63,37 @@ def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
 
     input_ids = batch_tokenize(prompt, tokenizer, pad_token_id=tokenizer.bos_id()).cuda()
     generate_kwargs = {}
+    set_seed()
+    output_ids = sequenece_generator.generate(
+        input_ids[:1],
+        num_return_sequences=generate_kwargs.get("num_return_sequences", 1),
+        max_length=generate_kwargs.get("max_length", 100),
+        num_beams=generate_kwargs.get("num_beams", 1),
+        do_sample=generate_kwargs.get("do_sample", True),
+        temperature=generate_kwargs.get("temperature", 1.0),
+        top_k=generate_kwargs.get("top_k", 50),
+        top_p=generate_kwargs.get("top_p", 1.0),
+        repetition_penalty=generate_kwargs.get("repetition_penalty", 1),
+        length_penalty=generate_kwargs.get("repetition_penalty", 1.0),
+    )
+    output_tokens = output_ids.tolist()
+    all_output_str = []
+    for b in range(len(output_tokens)):
+        for sent_idx in range(len(output_tokens[b])):
+            cur_output_tokens = output_tokens[b][sent_idx]
+            cur_sent = tokenizer.decode(cur_output_tokens)
+            all_output_str.append(cur_sent)
+    print(all_output_str)
+    gt = [
+        "user\nHow can I keep flys away from my house\nassistant\nThere are several ways to keep flies away from your\
+ house:\n\n1. Use citronella: Place citronella candles or essential oil in areas where flies likes to\
+ congregate. This can be a temporary solution,",
+        "Here are some tips to eliminate food sources, keep it's a lot of people eat. \nAlso, sprays with essential\
+ oils like garlic or vinegar. You can also put some sticky traps are also a very effective",
+    ]
+    assert all_output_str[0] == gt[0], all_output_str[0]
+
+    set_seed()
     output_ids = sequenece_generator.generate(
         input_ids,
         num_return_sequences=generate_kwargs.get("num_return_sequences", 1),
@@ -74,13 +115,6 @@ def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
             cur_sent = tokenizer.decode(cur_output_tokens)
             all_output_str.append(cur_sent)
     print(all_output_str)
-    gt = [
-        "user\nHow can I keep flys away from my house\nassistant\nThere are several ways to keep flies away from your\
- house:\n\n1. Use citronella: Place citronella candles or essential oil in areas where flies likes to\
- congregate. This can be a temporary solution,",
-        "Here are some tips to eliminate food sources, keep it's a lot of people eat. \nAlso, sprays with essential\
- oils like garlic or vinegar. You can also put some sticky traps are also a very effective",
-    ]
     assert all_output_str[0] == gt[0], all_output_str[0]
     assert all_output_str[1][len(prompt[1]) :] == gt[1], all_output_str[1][len(prompt[1]) :]
 
