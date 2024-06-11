b2f433d414db3cf1261907d86daa9ddfaecc1968
shidongxing <shidongxing@>
2024-06-11 11:55:41 +0800

    fix testcase

--- tests/test_infer/test_generate.py
+++ tests/test_infer/test_generate.py
@@ -24,7 +24,8 @@ def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
     model_wt = os.path.join(path, "model_tp0_pp0.pt")
     model_config = torch.load(model_cfg)
     model_config["apply_post_layer_norm"] = False
-    model_config.pop("adapt_hf")
+    if model_config.get("adapt_hf") is not None:
+        model_config.pop("adapt_hf")
     evo_cfg = dict(
         model_type=model_type,
         model=model_config,
@@ -38,6 +39,17 @@ def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
     initialize_distributed_env(evo_cfg, master_port=23574, args_check=False)
 
     tokenizer = SentencePieceProcessor(tokenizer_path)  # pylint: disable=E1121
+
+    def convert_to_str(output_ids):
+        output_tokens = output_ids.tolist()
+        all_output_str = []
+        for b in range(len(output_tokens)):
+            for sent_idx in range(len(output_tokens[b])):
+                cur_output_tokens = output_tokens[b][sent_idx]
+                cur_sent = tokenizer.decode(cur_output_tokens)
+                all_output_str.append(cur_sent)
+        return all_output_str
+
     model = initialize_model()
     _ = initialize_parallel_communicator(model)
     # Directly get the origin model without NativeAMP wrapper.
@@ -46,12 +58,6 @@ def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
     state_dict = torch.load(model_wt)
     load_info = model.load_state_dict(state_dict, strict=False)
     print(load_info)
-    prompt = [
-        "user\nHow can I keep flys away from my house\nassistant\n",
-        "user\nHow can I keep flys away from my house\nassistant\nThe best way is to keep your house clean, "
-        "and sweep away from where your meals are prepared, since flys tend to seek out food particles.\n"
-        "user\nAny other advice?\nassistant\n",
-    ]
 
     sequenece_generator = SequenceGenerator(
         decoder=model,
@@ -61,62 +67,57 @@ def load_and_generate(path, model_type="INTERNLM2_PUBLIC", tokenizer_path=""):
         additional_eos_token_list=None,
     )
 
-    input_ids = batch_tokenize(prompt, tokenizer, pad_token_id=tokenizer.bos_id()).cuda()
-    generate_kwargs = {}
-    set_seed()
-    output_ids = sequenece_generator.generate(
-        input_ids[:1],
-        num_return_sequences=generate_kwargs.get("num_return_sequences", 1),
-        max_length=generate_kwargs.get("max_length", 100),
-        num_beams=generate_kwargs.get("num_beams", 1),
-        do_sample=generate_kwargs.get("do_sample", True),
-        temperature=generate_kwargs.get("temperature", 1.0),
-        top_k=generate_kwargs.get("top_k", 50),
-        top_p=generate_kwargs.get("top_p", 1.0),
-        repetition_penalty=generate_kwargs.get("repetition_penalty", 1),
-        length_penalty=generate_kwargs.get("repetition_penalty", 1.0),
-    )
-    output_tokens = output_ids.tolist()
-    all_output_str = []
-    for b in range(len(output_tokens)):
-        for sent_idx in range(len(output_tokens[b])):
-            cur_output_tokens = output_tokens[b][sent_idx]
-            cur_sent = tokenizer.decode(cur_output_tokens)
-            all_output_str.append(cur_sent)
-    print(all_output_str)
-    gt = [
-        "user\nHow can I keep flys away from my house\nassistant\nThere are several ways to keep flies away from your\
- house:\n\n1. Use citronella: Place citronella candles or essential oil in areas where flies likes to\
- congregate. This can be a temporary solution,",
-        "Here are some tips to eliminate food sources, keep it's a lot of people eat. \nAlso, sprays with essential\
- oils like garlic or vinegar can help?\nTo Keep flies away:\nHere are some more tips",
+    test_prompt_0 = "Gold is considered to be a precious metal."
+    test_prompt_1 = "what is love? someone think it is a feeling, someone think it is a chemical reaction."
+    test_prompt_2 = "kobe bryant is a basketball player."
+
+    prompt_3 = [
+        test_prompt_0,
+        test_prompt_1,
+        test_prompt_2,
     ]
-    assert all_output_str[0] == gt[0], all_output_str[0]
+    prompt_2 = [
+        test_prompt_0,
+        test_prompt_1,
+    ]
 
-    set_seed()
-    output_ids = sequenece_generator.generate(
-        input_ids,
-        num_return_sequences=generate_kwargs.get("num_return_sequences", 1),
-        max_length=generate_kwargs.get("max_length", 100),
-        num_beams=generate_kwargs.get("num_beams", 1),
-        do_sample=generate_kwargs.get("do_sample", True),
-        temperature=generate_kwargs.get("temperature", 1.0),
-        top_k=generate_kwargs.get("top_k", 50),
-        top_p=generate_kwargs.get("top_p", 1.0),
-        repetition_penalty=generate_kwargs.get("repetition_penalty", 1),
-        length_penalty=generate_kwargs.get("repetition_penalty", 1.0),
+    prompt_1 = [test_prompt_0]
+
+    def generate(prompt):
+        input_ids = batch_tokenize(prompt, tokenizer, pad_token_id=tokenizer.bos_id()).cuda()
+        generate_kwargs = {}
+        set_seed()
+        output_ids = sequenece_generator.generate(
+            input_ids,
+            num_return_sequences=generate_kwargs.get("num_return_sequences", 1),
+            max_length=generate_kwargs.get("max_length", input_ids.shape[1] + 80),
+            num_beams=generate_kwargs.get("num_beams", 1),
+            do_sample=generate_kwargs.get("do_sample", False),
+            temperature=generate_kwargs.get("temperature", 1.0),
+            top_k=generate_kwargs.get("top_k", 50),
+            top_p=generate_kwargs.get("top_p", 1.0),
+            repetition_penalty=generate_kwargs.get("repetition_penalty", 1),
+            length_penalty=generate_kwargs.get("repetition_penalty", 1.0),
+        )
+
+        all_output_str = convert_to_str(output_ids)
+        return all_output_str
+
+    output_3 = generate(prompt_3)
+    output_2 = generate(prompt_2)
+    output_1 = generate(prompt_1)
+
+    assert output_3[0] == output_2[0]
+    assert output_3[1] == output_2[1]
+    assert (
+        output_1[0]
+        == "Gold is considered to be a precious metal. It is a metal that is highly valued for its \
+rarity and beauty. Gold is often used in jewelry, coins, and other decorative items. It is also used in \
+the production of electronics and other high-tech products. Gold is a highly sought-after metal because \
+of its ability to resist corrosion and tarnish. It is also highly resistant to fire and is a good conductor \
+of heat and electricity.\n"
     )
-
-    output_tokens = output_ids.tolist()
-    all_output_str = []
-    for b in range(len(output_tokens)):
-        for sent_idx in range(len(output_tokens[b])):
-            cur_output_tokens = output_tokens[b][sent_idx]
-            cur_sent = tokenizer.decode(cur_output_tokens)
-            all_output_str.append(cur_sent)
-    print(all_output_str)
-    assert all_output_str[0] == gt[0], all_output_str[0]
-    assert all_output_str[1][len(prompt[1]) :] == gt[1], all_output_str[1][len(prompt[1]) :]
+    print("test generate done!")
 
 
 def test_internlm2_1_8B_generate():
