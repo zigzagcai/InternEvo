0ad77b8b36446145eff478a697bf47c4035be32d
Huihuang Zheng <zhhsplendid@163.com>
2024-11-01 11:52:38 +0800

    Add z loss to PipelineSchedule (#365)
    Co-authored-by: shdlh <1245>

--- scripts/mulit_machine_mpi/prepare_env/install_inside_docker_muxi.sh
+++ scripts/mulit_machine_mpi/prepare_env/install_inside_docker_muxi.sh
@@ -1,6 +1,8 @@
 apt-get update
 apt-get install -y vim
 apt-get install -y git
+apt-get install -y gcc g++
+apt-get install -y libibverbs1   librdmacm1  ibverbs-providers  libibumad3 libibverbs-dev  librdmacm-dev ibverbs-utils libibumad-dev
 /opt/conda/bin/pip install \
   transformers \
   sentencepiece \
@@ -20,5 +22,6 @@ apt-get install -y git
   py-libnuma \
   pynvml \
   tensorboard \
+  h5py \
  -i https://pypi.tuna.tsinghua.edu.cn/simple
 
